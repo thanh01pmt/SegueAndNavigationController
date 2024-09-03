a4afe9abf9392f4ea516bc8aa6f1b72b47163268
Tony Pham <thanh01.pmt@gmail.com>
2024-09-04 04:55:22 +0700

    Initial Commit

---
+++ SegueAndNavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueAndNavigationController
+//
+//  Created by Tony Pham on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
